65f583ad5159fee1b97fc8ff5275772a1dadb690
Timothek2112 <93799063+Timothek2112@users.noreply.github.com>
2023-01-30 15:08:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ class PongPaddle(Widget):
 
     def bounce_ball(self, ball):
         if self.collide_widget(ball):
-            vx, vy = ball.velocity
+            velocity_X, velocity_Y = ball.velocity
             offset = (ball.center_y - self.center_y) / (self.height / 2)
-            bounced = Vector(-1 * vx, vy)
+            bounced = Vector(-1 * velocity_X, velocity_Y)
             vel = bounced * 1.1
             ball.velocity = vel.x, vel.y + offset
 
@@ -68,7 +68,7 @@ class PongApp(App):
     def build(self):
         game = PongGame()
         game.serve_ball()
-        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        Clock.schedule_interval(game.update, 1.0 / 120.0)
         return game
 
 
